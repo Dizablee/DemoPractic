4f2a9d0f403139e2a9d78b14ece9bc7ff56110ac
Dizablee <mth78931@gmail.com>
2026-03-04 12:43:27 +0300

    добавил форму авторизации

--- DemoForms/LoginForm.Designer.cs
+++ DemoForms/LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DemoForms
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(560, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- DemoForms/LoginForm.cs
+++ DemoForms/LoginForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DemoForms
 {
-    public partial class Form1: Form
+    public partial class LoginForm: Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
