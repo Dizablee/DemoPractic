b5ca9e7452c2b5297d2b8aa01457e03813a96bb7
Dizablee <mth78931@gmail.com>
2026-03-04 12:28:49 +0300

    добавил фаийлы проекта

---
+++ DemoLib/User/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib
+{
+    public class User
+    {
+        public string role { get; set; }
+        public string name { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+
+        public User(string role_, string name_, string login_, string password_)
+        {
+            role = role_;
+            name = name_;
+            login = login_;
+            password = password_;
+        }
+    }
+}
